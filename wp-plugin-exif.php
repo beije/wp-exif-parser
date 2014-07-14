2549a94012bd39012cd3abad6a0287b919e07dc6
beije <git@benjaminhorn.se>
2014-07-14 09:17:11 +0200

    Changed version number.

--- wp-plugin-exif.php
+++ wp-plugin-exif.php
@@ -3,7 +3,7 @@
 Plugin Name: wp-plugin-exif
 Plugin URI: http://www.benjaminhorn.se
 Description: Parses the exif data from images.
-Version: 0.0.3
+Version: 0.0.0
 Author: Benjamin Horn
 Author URI: http://www.benjaminhorn.se
 */
