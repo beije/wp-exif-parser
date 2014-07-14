8a4a3508334b2299a11065f1bfd7e728bbe1724d
beije <git@benjaminhorn.se>
2014-07-14 09:16:57 +0200

    Initial commit.

---
+++ wp-plugin-exif.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: wp-plugin-exif
+Plugin URI: http://www.benjaminhorn.se
+Description: Parses the exif data from images.
+Version: 0.0.3
+Author: Benjamin Horn
+Author URI: http://www.benjaminhorn.se
+*/
+
+
+?>
